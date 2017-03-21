08bee8fc4a7d35dcf00d3e9a92bcdcb1d6b1b64d
小飼慎一 <kogai0121@gmail.com>
2017-03-21 21:31:30 +0900

    Drop generic, use Enum

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,49 +1,37 @@
-use std::fmt::Display;
 use ast::{Node, Nodes};
-use object::{Object, ObjectType, Objects, Null};
+use object::{Object, ObjectType, Null};
 
-pub fn eval<T: Node>(node: T) -> Objects {
+pub fn eval<T: Node>(node: T) -> Object {
     match node.to_enum() {
-        Nodes::IntegerLiteral(n) => {
-            Objects::Integer(Object::<usize> {
-                                 object_type: ObjectType::Integer,
-                                 value: n.value,
-                             })
-        }
-        _ => {
-            Objects::Null(Object::<Null> {
-                              object_type: ObjectType::Null,
-                              value: Null,
-                          })
-        }
+        Nodes::IntegerLiteral(n) => Object { object_type: ObjectType::Integer(n.value) },
+        Nodes::Boolean(n) => Object { object_type: ObjectType::Boolean(n.value) },
+        _ => Object { object_type: ObjectType::Null(Null) },
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fmt::Debug;
-    use std::cmp::PartialEq;
     use lexer;
     use parser;
 
-    fn test_eval<T: Display + PartialEq + Debug>(input: String) -> Objects {
+    fn test_eval(input: String) -> Object {
         let l = lexer::new(input);
         let mut parser = parser::new(l);
         let program = parser.parse_program();
         eval(program)
     }
 
-    fn test_object<T: Display + PartialEq + Debug>(obj: Object<T>, expected: T) {
-        assert_eq!(obj.value, expected);
+    fn test_object(obj: Object, expected: String) {
+        assert_eq!(obj.inspect(), expected);
     }
 
     #[test]
     fn it_should_evaluate_integer_expression() {
-        let expects = [("5", 5), ("10", 10)];
+        let expects = [("5", "5"), ("10", "10")];
         for expect in expects.iter() {
-            let result = test_eval::<usize>(expect.0.to_string()).to_concrete_usize().unwrap();
-            test_object(result, expect.1);
+            let result = test_eval(expect.0.to_string());
+            test_object(result, expect.1.to_string());
         }
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,13 +1,6 @@
 use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug, Clone)]
-pub enum ObjectType {
-    Integer,
-    Boolean,
-    Null,
-}
-
-#[derive(Debug, Clone)]
 pub struct Null;
 
 impl Display for Null {
@@ -16,54 +9,25 @@ impl Display for Null {
     }
 }
 
-#[derive(Debug)]
-pub enum Objects {
-    Integer(Object<usize>),
-    Boolean(Object<bool>),
-    Null(Object<Null>),
-}
-
-impl Objects {
-    pub fn to_concrete_usize(&self) -> Option<Object<usize>> {
-        match *self {
-            Objects::Integer(ref x) => Some(x.clone()),
-            _ => None,
-        }
-    }
-
-    pub fn to_concrete_bool(&self) -> Option<Object<bool>> {
-        match *self {
-            Objects::Boolean(ref x) => Some(x.clone()),
-            _ => None,
-        }
-    }
-
-    pub fn to_concrete_null(&self) -> Option<Object<Null>> {
-        match *self {
-            Objects::Null(ref x) => Some(x.clone()),
-            _ => None,
-        }
-    }
-
-    pub fn to_concrete_type<T: Display>(&self) -> Object<T> {
-        // let f = match *self {
-        //     Objects::Integer(x) => x,
-        //     Objects::Boolean(x) => x,
-        //     Objects::Null(x) => x,
-        // };
-        unimplemented!();
-    }
+#[derive(Debug, Clone)]
+pub enum ObjectType {
+    Integer(usize),
+    Boolean(bool),
+    Null(Null),
 }
 
 #[derive(Debug, Clone)]
-pub struct Object<T: Display> {
+pub struct Object {
     pub object_type: ObjectType,
-    pub value: T,
 }
 
-impl<T: Display> Object<T> {
+impl Object {
     pub fn inspect(&self) -> String {
-        format!("{}", self.value)
+        match self.object_type {
+            ObjectType::Integer(ref x) => format!("{}", x),
+            ObjectType::Boolean(ref x) => format!("{}", x),
+            ObjectType::Null(ref x) => format!("{}", x),
+        }
     }
 }
 
