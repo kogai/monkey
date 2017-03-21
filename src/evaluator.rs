851b0ea3e02978ab1cbaeab466a0a862f2c90ebb
小飼慎一 <kogai0121@gmail.com>
2017-03-21 22:29:30 +0900

    implements foundation of eval

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,12 +1,20 @@
-use ast::{Node, Nodes};
+use ast::{Node, Nodes, Statement};
 use object::{Object, ObjectType, Null};
 
-pub fn eval<T: Node>(node: T) -> Object {
-    match node.to_enum() {
-        Nodes::IntegerLiteral(n) => Object { object_type: ObjectType::Integer(n.value) },
-        Nodes::Boolean(n) => Object { object_type: ObjectType::Boolean(n.value) },
+pub fn eval(node: Nodes) -> Object {
+    use self::Nodes::*;
+    match node {
+        Program(x) => eval_statements(&x.statements),
+        ExpressionStatement(x) => eval(x.expression.to_enum()),
+        IntegerLiteral(n) => Object { object_type: ObjectType::Integer(n.value) },
+        Boolean(n) => Object { object_type: ObjectType::Boolean(n.value) },
         _ => Object { object_type: ObjectType::Null(Null) },
     }
+}
+
+pub fn eval_statements(statements: &Vec<Box<Statement>>) -> Object {
+    let statement = statements.first().unwrap();
+    eval(statement.to_enum())
 }
 
 #[cfg(test)]
@@ -19,7 +27,7 @@ mod tests {
         let l = lexer::new(input);
         let mut parser = parser::new(l);
         let program = parser.parse_program();
-        eval(program)
+        eval(program.to_enum())
     }
 
     fn test_object(obj: Object, expected: String) {
