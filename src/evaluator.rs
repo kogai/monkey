715a3327b815f496b91dbc3e84de12ed6613c9b6
小飼慎一 <kogai0121@gmail.com>
2017-03-22 22:33:08 +0900

    implement evaluation for infix operator

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -17,6 +17,12 @@ pub fn eval(node: Nodes) -> Object {
             let right = eval(x.right.to_enum());
             eval_prefix_expression(operator, right)
         }
+        InfixExpression(x) => {
+            let operator = x.operator.clone();
+            let left = eval(x.left.to_enum());
+            let right = eval(x.right.to_enum());
+            eval_infix_expression(operator, left, right)
+        }
         _ => NULL,
     }
 }
@@ -25,6 +31,34 @@ fn native_bool_to_boolean_obj(x: bool) -> Object {
     match x {
         true => TRUE,
         false => FALSE,
+    }
+}
+
+fn eval_infix_expression(operator: String, left: Object, right: Object) -> Object {
+    if let ObjectType::Integer(l) = left.object_type {
+        if let ObjectType::Integer(r) = right.object_type {
+            return eval_integer_infix_expression(operator, l, r);
+        }
+    }
+
+    match operator.as_str() {
+        "==" => native_bool_to_boolean_obj(left == right),
+        "!=" => native_bool_to_boolean_obj(left != right),
+        _ => NULL,
+    }
+}
+
+fn eval_integer_infix_expression(operator: String, left: i32, right: i32) -> Object {
+    match operator.as_str() {
+        "+" => Object::new_i32(left + right),
+        "-" => Object::new_i32(left - right),
+        "*" => Object::new_i32(left * right),
+        "/" => Object::new_i32(left / right),
+        "<" => native_bool_to_boolean_obj(left < right),
+        ">" => native_bool_to_boolean_obj(left > right),
+        "==" => native_bool_to_boolean_obj(left == right),
+        "!=" => native_bool_to_boolean_obj(left != right),
+        _ => NULL,
     }
 }
 
@@ -74,7 +108,21 @@ mod tests {
 
     #[test]
     fn it_should_evaluate_integer_expression() {
-        let expects = [("5", 5), ("10", 10), ("-5", -5), ("-10", -10)];
+        let expects = [("5", 5),
+                       ("10", 10),
+                       ("-5", -5),
+                       ("-10", -10),
+                       ("5 + 5 + 5 + 5 - 10", 10),
+                       ("2 * 2 * 2 * 2 * 2", 32),
+                       ("-50 + 100 + -50", 0),
+                       ("5 * 2 + 10", 20),
+                       ("5 + 2 * 10", 25),
+                       ("20 + 2 * -10", 0),
+                       ("50 / 2 * 2 + 10", 60),
+                       ("2 * (5 + 10)", 30),
+                       ("3 * 3 * 3 + 10", 37),
+                       ("3 * (3 * 3) + 10", 37),
+                       ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50)];
         for expect in expects.iter() {
             let result = test_eval(expect.0.to_string());
             assert_eq!(result.to_i32().unwrap(), expect.1);
@@ -83,7 +131,25 @@ mod tests {
 
     #[test]
     fn it_should_evaluate_boolean_expression() {
-        let expects = [("true", true), ("false", false)];
+        let expects = [("true", true),
+                       ("false", false),
+                       ("1 < 2", true),
+                       ("1 > 2", false),
+                       ("1 < 1", false),
+                       ("1 > 1", false),
+                       ("1 == 1", true),
+                       ("1 != 1", false),
+                       ("1 == 2", false),
+                       ("1 != 2", true),
+                       ("true == true", true),
+                       ("false == false", true),
+                       ("true == false", false),
+                       ("true != false", true),
+                       ("false != true", true),
+                       ("(1 < 2) == true", true),
+                       ("(1 < 2) == false", false),
+                       ("(1 > 2) == true", false),
+                       ("(1 > 2) == false", true)];
         for expect in expects.iter() {
             let result = test_eval(expect.0.to_string());
             assert_eq!(result.to_bool().unwrap(), expect.1);
